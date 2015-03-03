51803caff09071bd6517a9a67fdf8c6b00d07dec
Tom Henderson <tomhenderson@mac.com>
2015-03-03 22:38:39 +1300

    Add non ModelForm BootStrapForm.

--- dominion/common/forms.py
+++ dominion/common/forms.py
@@ -1,7 +1,21 @@
 from django import forms
 
 
-class BootStrapForm(forms.ModelForm):
+class BootStrapModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(BootStrapModelForm, self).__init__(*args, **kwargs)
+
+        # Add bootstrap classes:
+        for field in self.fields:
+            checkbox_fields = [
+                forms.CheckboxInput,
+                forms.CheckboxSelectMultiple,
+            ]
+            if type(self.fields[field].widget) not in checkbox_fields:
+                self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+class BootStrapForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(BootStrapForm, self).__init__(*args, **kwargs)
 
